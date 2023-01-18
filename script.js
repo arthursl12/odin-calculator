a0bfd755ee8a5b12a0be0ee21fef1b5d9b56ba06
arthursl12 <arthursoutolima@gmail.com>
2023-01-18 13:37:30 -0300

    Create operate function

--- script.js
+++ script.js
@@ -3,9 +3,21 @@ function sub(a,b){ return a-b; }
 function mul(a,b){ return a*b; }
 function div(a,b){ if(b!=0){ return a/b; }else{ return "DIV"; }}
 
+function operate(op,a,b){
+    return op(a,b);
+}
+
+
 
 console.log(add(5,3));
 console.log(sub(5,3));
 console.log(mul(5,3));
 console.log(div(5,3));
 console.log(div(5,0));
+
+console.log(operate(add,5,3));
+console.log(operate(sub,5,3));
+console.log(operate(mul,5,3));
+console.log(operate(div,5,3));
+console.log(operate(div,5,0));
+
